34abe9e94c7bf68d588b5d0ee7f4a5ba4b382b3f
dcl525 <dcl525@gmail.com>
2021-06-14 12:25:41 +0000

    Merged PR 75: AspNetCore Configuration Extensions
    adding aspnetcore configuration extensions

---
+++ Common.Utilities.AspNetCore/Extensions/AspNetCoreConfigurationExtensions.cs
@@ -0,0 +1,54 @@
+﻿/* Copyright (C) 2012, 2013 Dan Leonard
+ * 
+ * This is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ */
+
+using Common.Utilities.Authentication.DependencyInjection.Exports;
+using Common.Utilities.DependencyInjection.Exports.Types.Abstractions;
+using Common.Utilities.DependencyInjection.Registration;
+using Common.Utilities.UserManagement.DependencyInjection.Exports;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Utilities.AspNetCore.Extensions
+{
+		public static class AspNetCoreConfigurationExtensions
+		{
+				public static void ConfigureAspNetCoreServices<TDependencyExports>(this IServiceCollection serviceDescriptors) where TDependencyExports : IDependencyExport
+				{
+						var configuration = GetConfiguration();
+
+						var exportRegistration = new DependencyExportRegistration();
+
+						// Internal dependencies
+
+						exportRegistration.RegisterDependencies<AuthenticationDependencyExports>(serviceDescriptors, configuration);
+						exportRegistration.RegisterDependencies<UserManagementDependencyExports>(serviceDescriptors, configuration);
+
+						// App defined dependencies
+
+						exportRegistration.RegisterDependencies<TDependencyExports>(serviceDescriptors, configuration);
+
+						// Swagger dependencies
+
+						serviceDescriptors.AddSwaggerGen();
+				}
+
+				public static IConfiguration GetConfiguration()
+				{
+						var configuration = new ConfigurationBuilder()
+								.AddJsonFile("appsettings.json")
+								.Build();
+
+						return configuration;
+				}
+		}
+}
