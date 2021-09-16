2d1252602e812c5df7089a8e83fa53f1a85552ab
dcl525 <dcl525@gmail.com>
2021-09-16 19:12:29 +0000

    Merged PR 183: ToResponse StatusCode Overload
    adding ToResponse overload

--- Common.Utilities.AspNetCore/Extensions/AspNetCoreResponseExtensions.cs
+++ Common.Utilities.AspNetCore/Extensions/AspNetCoreResponseExtensions.cs
@@ -14,14 +14,27 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Net;
 
 namespace Common.Utilities.AspNetCore.Response.Extensions
 {
+		// TODO: Move these to Common.Utilities.Extensions
+
 		public static class AspNetCoreResponseExtensions
 		{
 				public static IActionResult ToResponse(this object obj)
 				{
 						var response = new ObjectResult(obj);
+
+						return response;
+				}
+
+				public static IActionResult ToResponse(this object obj, HttpStatusCode httpStatusCode)
+				{
+						var response = new ObjectResult(obj)
+						{
+								StatusCode = (int)httpStatusCode
+						};
 
 						return response;
 				}
